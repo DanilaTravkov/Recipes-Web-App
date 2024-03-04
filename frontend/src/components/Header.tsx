43cfbac0aba3ba099a681c7571f016b84d9a0e80
danila <danila@MacBook-Air-danila.local>
2024-03-04 15:08:24 +0100

    Navigation menu reworked

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,58 +4,58 @@ import '../override.css'
 import React, { useState } from 'react'
 import { NavItem, NavWindow } from './NavItem'
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-]
+// const components: { title: string; href: string; description: string }[] = [
+//   {
+//     title: "Alert Dialog",
+//     href: "/docs/primitives/alert-dialog",
+//     description:
+//       "A modal dialog that interrupts the user with important content and expects a response.",
+//   },
+//   {
+//     title: "Hover Card",
+//     href: "/docs/primitives/hover-card",
+//     description:
+//       "For sighted users to preview content available behind a link.",
+//   },
+//   {
+//     title: "Progress",
+//     href: "/docs/primitives/progress",
+//     description:
+//       "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+//   },
+//   {
+//     title: "Scroll-area",
+//     href: "/docs/primitives/scroll-area",
+//     description: "Visually or semantically separates content.",
+//   },
+//   {
+//     title: "Tabs",
+//     href: "/docs/primitives/tabs",
+//     description:
+//       "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+//   },
+//   {
+//     title: "Tooltip",
+//     href: "/docs/primitives/tooltip",
+//     description:
+//       "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+//   },
+// ]
 
 export function MyNavigationMenu() {
   return <>
 		<ul className='flex justify-center space-x-8'>
 			<NavItem title="About">
-				<NavWindow text='Something about this site' />
+				<NavWindow items={["About", "Blyat", "Cyka"]} />
 			</NavItem>
 			<NavItem title="Daily">
-				<NavWindow text='Something about daily options' />
+				<NavWindow items={["About", "Blyat", "Cyka"]} />
 			</NavItem>
 			<NavItem title="Categories">
-				<NavWindow text='Choose categories' />
+				<NavWindow items={["About", "Blyat", "Cyka"]} />
 			</NavItem>
 			<NavItem title="Policy">
-				<NavWindow text='Read the policy' />
+				<NavWindow items={["About", "Blyat", "Cyka"]} />
 			</NavItem>
 		</ul>
 	</>
--- frontend/src/components/NavItem.tsx
+++ frontend/src/components/NavItem.tsx
@@ -1,33 +1,35 @@
 import React, { useState } from 'react'
 import '../custom.css'
 
-export const NavWindow = ({text}: {text: string}) => {
-	return <div className="absolute bg-white/10 p-5 mt-2 rounded">{text}</div>
+export const NavWindow = ({items}: {items: string[]}) => {
+	return (
+    <ul className="absolute mt-2 rounded w-24">
+      {
+        items.map((item, key) => (
+          <li className='py-2 w-2/3' key={key}>
+            {item}
+          </li>
+        ))
+      }
+    </ul>
+  ) 
 }
 
 export const NavItem = ({ title, children }: { title: string; children: React.ReactNode }) => {
-  const [hovered, setHovered] = useState(false);
-
-  const handleHover = () => {
-    setHovered(true);
-  };
-
-  const handleMouseOut = () => {
-    setHovered(false);
-  };
+  const [hoveredHeader, setHoveredHeader] = useState(false);
 
   return (
     <>
       <li className='transition-colors ease-in-out cursor-default'>
         <div 
 					className='flex items-center justify-center'
-					onMouseOver={handleHover}
-        	onMouseOut={handleMouseOut}
+					onMouseOver={() => setHoveredHeader(true)}
+          onMouseOut={() => setHoveredHeader(true)}
 				>
           <p className="hover:text-white/70">{title}</p>
-          <img className={`ml-2 w-3 h-3 transform ${hovered ? 'rotate-180' : 'rotate-0'} transition-transform ease-in-out duration-300`} src="src/assets/other/arrow-down.png" alt="arrow" />
+          <img className={`ml-2 w-3 h-3 transform ${hoveredHeader ? 'rotate-180' : 'rotate-0'} transition-transform ease-in-out duration-300`} src="src/assets/other/arrow-down.png" alt="arrow" />
         </div>
-        <div className={`transition-opacity duration-300 ${hovered ? 'opacity-1' : 'opacity-0'}`}>
+        <div onMouseOver={() => setHoveredHeader(true)} onMouseOut={() => setHoveredHeader(false)} className={`transition-opacity duration-300 ${hoveredHeader ? 'block opacity-1' : 'opacity-0'}`}>
           {children}
         </div>
       </li>
